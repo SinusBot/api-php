be3e1a22c61e2fb504dddbcad8e4d072a9171a87
marburger93 <marburger93@arcor.de>
2015-07-26 18:59:14 +0200

    getFiles.php

---
+++ examples/getFiles.php
@@ -0,0 +1,10 @@
+<?php
+include("sinusbot.class.php");
+$sinusbot = new SinusBot("127.0.0.1", 8087);
+$sinusbot->login("admin", "foobar");
+
+$files = $sinusbot->getFiles();
+for ($i = 0; $i < count($files); $i++) {
+  echo $files[$i]['uuid'].': '.$files[$i]['artist'].' - '.$files[$i]['title'].' ('.$files[$i]['album'].')<br>';
+}
+?>
